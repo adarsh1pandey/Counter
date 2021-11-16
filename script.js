808925378360d920acb1f6f447f6a46173582350
adarshpnd71 <adarshpnd71@gmail.com>
2021-11-16 15:09:03 +0530

    make a counter button to count using simple html, css and javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+let myAge = 19
+console.log(myAge)
+let humanDogRatio = 10
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge)
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50 ;
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75;
+bonusPoints = bonusPoints + 45;
+
+console.log(bonusPoints)
+
+*/
+let countdown = document.getElementById("countEl")
+console.log(countdown)
+let count = 0
+function increment(){
+    count = count + 1
+    countdown.innerText = count
+    console.log(count)
+}
+
+
+
+
+//initialize the count as 0
+
+// listen to the clicks on the increment button 
+
+//increment the count on clicking the button 
+
+//change the count in html to reflect the new value of count 
